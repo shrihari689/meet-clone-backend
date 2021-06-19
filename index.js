76f9efe4596495298736cfc9874875824578d216
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-06-19 21:05:57 +0530

    ⏰ Add Time to Message

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const http = require("http");
+const { getDateTimeString } = require("./utils")
 const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: ['http://localhost:3000', "https://meet-clone-shrihari689.web.app"],
+        origin: [
+            'http://localhost:3000',
+            "https://meet-clone-shrihari689.web.app"
+        ],
     }
 });
 const PORT = process.env.PORT || 4000;
@@ -18,7 +22,9 @@ io.on("connection", (socket) => {
         console.log("Meet ID: " + meetId)
     })
     socket.on("newMessage", (data) => {
-        io.to(socket.meetId).emit("newMessage", data)
+        const message = JSON.parse(data);
+        const newMessage = JSON.stringify({ ...message, time: getDateTimeString().time })
+        io.to(socket.meetId).emit("newMessage", newMessage)
     })
     socket.on("disconnect", () => {
         console.log("Disconnected: " + socket.id)
